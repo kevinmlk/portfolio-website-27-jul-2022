eaaab72c24031b53885be5fa046113d4cd4174e2
Kevin Malekera <r0900654@student.thomasmore.be>
2024-10-15 21:44:25 +0200

    Added hotjar script for analytics

--- index.php
+++ index.php
@@ -30,6 +30,17 @@
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-KZPWFDM4');</script>
         <!-- End Google Tag Manager -->
+        <!--  Hotjar Tracking Code for https://kevinmalekera.be -->
+        <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:5169058,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
         <!-- Open Graph -->
         <meta property="og:title" content="Kevin Malekera - Student Digital Experience Design">
         <meta property="og:description" content="Hey, I'm Kevin Malekera a Digital Experience Design student at the University College Thomas More in Mechelen.">
