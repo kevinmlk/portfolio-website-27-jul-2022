19eaa2fb2a30d5cf767b80de785bfe2c79dc3779
Kevin Malekera <r0900654@student.thomasmore.be>
2024-10-15 16:38:28 +0200

    feat: added an alt work webpage with an alternative case slideshow

---
+++ work-alt.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-H55DCDM7PL"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);} 
+            gtag('js', new Date());
+
+            gtag('config', 'G-H55DCDM7PL');
+        </script> -->
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KZPWFDM4');</script>
+        <!-- End Google Tag Manager -->
+        <!-- Open Graph -->
+        <meta property="og:title" content="Kevin Malekera - Recent projects">
+        <meta property="og:description" content="Welcome to my portfolio, here you can see an overview of my recent projects.">
+        <meta property="og:image" content="https://kevinmalekera.be/assets/media/work.jpg">
+        <meta property="og:url" content="https://kevinmalekera.be/work.php">
+        <meta property="og:type" content="website">
+        <!-- Tab title -->
+        <title>Portfolio | Kevin Malekera</title>
+        <!-- SEO -->
+        <meta name="title" content="Kevin Malekera - My portfolio">
+        <meta name="description" content="Here you can see all my realizations and projects. Click on a case to learn more about it.">
+        <meta name="keywords" content="kevin, malekera, Kevin Malekera, portfolio, work, web, contact, case, websites, project, projects, web development">
+        <meta name="robots" content="index, follow">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="author" content="Kevin Malekera">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Language meta tags -->
+        <link rel="alternate" hreflang="en-gb" href="https://kevinmalekera.be/work.php">
+        <link rel="alternate" hreflang="en-us" href="https://kevinmalekera.be/work.php">
+        <link rel="alternate" hreflang="en" href="https://kevinmalekera.be/work.php">
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="180x180" href="./assets/media/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./assets/media/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./assets/media/favicon/favicon-16x16.png">
+        <link rel="manifest" href="./assets/media/favicon/site.webmanifest">
+        <link rel="mask-icon" href="./assets/media/favicon/safari-pinned-tab.svg" color="#000000">
+        <meta name="msapplication-TileColor" content="#000000">
+        <meta name="theme-color" content="#ffffff">
+        <!-- CSS files -->
+        <link rel="stylesheet" href="./assets/css/reset.css">
+        <link rel="stylesheet" href="./assets/css/template.css">
+        <link rel="stylesheet" href="./assets/css/screen.css">
+        <link rel="stylesheet" href="./assets/css/responsive.css">
+        <!-- Google fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <!-- Playfair Display Font -->
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+        <!-- Open Sans Font -->
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+        <!-- Glide.js -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@glidejs/glide/dist/css/glide.core.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
+    </head>
+    <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KZPWFDM4"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        <!-- Include preloader -->
+        <?php include_once(__DIR__ . '/includes/preloader.inc.php'); ?>
+        <!-- Header -->
+        <header class="overlay alt-header work-header">
+            <div id="navigation" class="fw-container">
+                <h2><a href="./index.php" class="strikethrough" id="first-header"><b>Kevin</b> Malekera</a></h2>
+                <h2><a href="./index.php" class="strikethrough" id="second-header"><b>K</b>M</a></h2>
+                <!-- Hamburger menu -->
+                <div class="menu-wrap">
+                    <input type="checkbox" class="toggler">
+                    <div class="hamburger"><div></div></div>
+                    <div class="menu">
+                        <div>
+                            <div>
+                                <ul>
+                                    <li><a href="./work.php" class="underline-hover-white">Work</a></li>
+                                    <li><a href="./resume.php" class="underline-hover-white">Resume</a></li>
+                                    <li><a href="./about.php" class="underline-hover-white">About</a></li>
+                                    <li><a href="./contact.php" class="underline-hover-white">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Main navigation -->
+                <nav>
+                    <ul>
+                        <li><a href="./work.php" class="active">Work</a></li>
+                        <li><a href="./resume.php" class="underline-hover-white">Resume</a></li>
+                        <li><a href="./about.php" class="underline-hover-white">About</a></li>
+                        <li><a href="./contact.php" class="underline-hover-white">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+            <div class="parallax-content">
+                <h1>My portfolio</h1>
+                <p>Below is a selection of my most recent projects</p>
+            </div>
+            
+            <div></div>
+        </header>
+        <!-- Main content -->
+        <main>
+            <!-- Work introduction section -->
+            <section class="white-bg">
+                <div class="fw-container work-introduction">
+                    <div class="reveal fade-bottom">
+                        <h3>A digital designer specializing in web development</h3>
+                        <div class="line grey"></div>
+                        <p>
+                        Trained in IT and graphic design, I specialize in web development. So my main realizations are website and web shop that I have made for clients or projects during my various apprenticeships with large companies. Feel free to take a look and click on a case that appeals to you so you can get more information about it and see my way of working. Each case has its own web page explaining the working process.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <!-- Work gallery section -->
+            <section class="black-bg">
+                <div class="fw-container">
+                  <h3>All my completed accomplishments</h3>
+                  <div class="line coral"></div>
+                  
+                    <!-- Glide.js container -->
+                    <div class="glide">
+                        <div class="glide__track" data-glide-el="track">
+                            <ul class="glide__slides">
+                                <li class="glide__slide"><a href="./case/jbd-engineering.php"><img src="./assets/media/jbd-engineering-thumbnail.png" alt="Jbd Engineering website"></a></li>
+                                <li class="glide__slide"><a href="./case/belcoast.php"><img src="./assets/media/belcoast-thumbnail.png" alt="Jbd Engineering website"></a></li>
+                            </ul>
+                        </div>
+
+                        <div data-glide-el="controls" class="glide__arrows">
+                            <button data-glide-dir="<<">previous</button>
+                            <button data-glide-dir=">>">next</button>
+                        </div>
+
+                    </div>
+
+                </div>
+            </section>
+            
+            <!-- Include alt parallax -->
+            <?php include_once(__DIR__ . '/includes/parallax-alt.inc.php'); ?>
+        </main>
+        <!-- Include footer -->
+        <?php include_once(__DIR__ . '/includes/footer.inc.php'); ?>
+        <!-- Glide.js -->
+        <script>
+            let glide = new Glide('.glide', {
+            type: 'carousel',
+            startAt: 0,
+            perView: 1,
+            autoplay: 2500,
+            rewind: true,
+            });
+
+            glide.mount();
+
+
+        </script>
+        <!-- Back to top js -->
+        <script src="./assets/js/back-to-top.js" defer></script>
+        <!-- Font awesome link -->
+        <script src="https://kit.fontawesome.com/13ba0ff1d8.js" crossorigin="anonymous" defer></script>
+        <!-- jQuery script -->
+        <script src="./assets/js/jquery-3.6.0.min.js"></script>
+        <!-- Fade-in script -->
+        <script src="./assets/js/fade-in.js" defer></script>
+        <!-- Preloader -->
+        <script src="./assets/js/preloader.js"></script>
+    </body>
+</html>
